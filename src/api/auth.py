8ba9f24ac19d28f4e3a83b4685a73557364a486a
luciepienne <lucie.pienne.devia@gmail.com>
2024-11-08 17:16:40 +0100

    feat : API authentification ok

---
+++ src/api/auth.py
@@ -0,0 +1,100 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+# Configuration des constantes
+SECRET_KEY = "password" 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Initialisation du contexte de hachage des mots de passe
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Simuler une base de données d'utilisateurs
+fake_users_db = {
+    "ADMIN": {
+        "username": "admin",
+        "full_name": "Admin Master",
+        "email": "master@example.com",
+        "hashed_password": pwd_context.hash("admin")
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class User(BaseModel):
+    username: str
+
+def create_access_token(data: dict) -> str:
+    """
+    Create a new access token.
+    
+    Args:
+        data (dict): Data to encode in the token.
+
+    Returns:
+        str: Encoded JWT token.
+    """
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def authenticate_user(fake_db, username: str, password: str):
+    """
+    Authenticate user against fake database.
+    
+    Args:
+        fake_db (dict): Fake user database.
+        username (str): Username to authenticate.
+        password (str): Password to authenticate.
+
+    Returns:
+        User or False: User object if authentication is successful; False otherwise.
+    """
+    
+    user = fake_db.get(username)
+    
+    if not user or not pwd_context.verify(password, user['hashed_password']):
+        return False
+    
+    return User(username=username)
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+    """
+    Get the current user from the token.
+
+    Args:
+        token (str): JWT token.
+
+    Returns:
+        User: User object.
+
+    Raises:
+        HTTPException: If credentials are invalid.
+    """
+    
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        
+        if username is None:
+            raise credentials_exception
+        
+        return User(username=username)
+    
+    except JWTError:
+        raise credentials_exception
--- src/model_training/xg_boost.py
+++ src/model_training/xg_boost.py
@@ -1,25 +1,30 @@
-import sys
-import os
-import numpy as np
 import json
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import os
+import sys
 
-from data_preprocessing.preprocess_and_split import preprocess_data
-from sklearn.metrics import classification_report
-from xgboost import XGBClassifier
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pickle
 
+from sklearn.metrics import classification_report
+from xgboost import XGBClassifier
+
+from data_preprocessing.preprocess_and_split import preprocess_data
+
 # Charger et prétraiter les données
-X_train, X_test, y_train, y_test, feature_names, feature_types, encoding_dict = preprocess_data()
+X_train, X_test, y_train, y_test, feature_names, feature_types, encoding_dict = (
+    preprocess_data()
+)
 
 xgb_model = XGBClassifier(
-    objective='binary:logistic',
+    objective="binary:logistic",
     learning_rate=0.01,
     n_estimators=350,
     max_depth=3,
     random_state=42,
-    max_features='sqrt',
+    max_features="sqrt",
 )
 
 xgb_model.fit(X_train, y_train)
@@ -27,26 +32,31 @@ xgb_model.fit(X_train, y_train)
 y_train_predict = xgb_model.predict(X_train)
 y_test_predict = xgb_model.predict(X_test)
 
-print("=================================================================================================")
+print(
+    "================================================================================================="
+)
 print("Classification Report for XGBoost Model (Train Set):")
 print(classification_report(y_train, y_train_predict))
 
-print("=================================================================================================")
+print(
+    "================================================================================================="
+)
 print("Classification Report for XGBoost Model (Test Set):")
 print(classification_report(y_test, y_test_predict))
 
-with open('models/xgboost_model.pkl', 'wb') as f:
+with open("models/xgboost_model.pkl", "wb") as f:
     pickle.dump(xgb_model, f)
 
 # Sauvegarder les informations sur les features dans un fichier JSON
 feature_info = {
-    'feature_names': feature_names,
-    'feature_types': {name: str(dtype) for name, dtype in feature_types.items()},
-    'encoding_dict': {
+    "feature_names": feature_names,
+    "feature_types": {name: str(dtype) for name, dtype in feature_types.items()},
+    "encoding_dict": {
         key: {str(k): int(v) for k, v in value.items()}
         for key, value in encoding_dict.items()
-    }
+    },
 }
+
 
 def convert_to_json_serializable(obj):
     if isinstance(obj, np.integer):
@@ -58,10 +68,13 @@ def convert_to_json_serializable(obj):
     else:
         return obj
 
-with open('models/feature_info_xgboost.json', 'w') as f:
+
+with open("models/feature_info.json", "w") as f:
     json.dump(feature_info, f, indent=2, default=convert_to_json_serializable)
 
 print("Noms des features:", feature_names)
 print("Types des features:", feature_types)
 print("Dictionnaire d'encodage:", encoding_dict)
-print("Modèle entraîné et sauvegardé avec succès. Informations sur les features sauvegardées dans feature_info.json.")
+print(
+    "Modèle entraîné et sauvegardé avec succès. Informations sur les features sauvegardées dans feature_info.json."
+)
